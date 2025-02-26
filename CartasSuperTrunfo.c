0bae8d084f269324a05f6ad07c4535a533f716d6
m452-code <theus_1998@icloud.com>
2025-02-26 09:33:04 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,20 @@
 //Teste larissa
 
 int main() {
+       char codigo[20]  = "B01";
+    char nome[20] = " Rio de Janeiro";
+    int populacao = "6748000";
+    float area =  1200.25 ;
+    float PIB = 300.50 ;
+    int Pontos  = 30;  
+
+    printf("coidgo: %s\n", codigo );
+    printf(" nome: %s\n",nome); 
+    printf("populacao: %d populacao\n", populacao );
+    printf("area: %.2f km²\n", area );
+    printf("PIB: %.2f reais\n", PIB );
+    printf("Pontos Turísticos: %d\n",Pontos );
+
     char codigo[20]  = "A01";
     char nome[20] = "São Paulo";
     int populacao = "12325000";
